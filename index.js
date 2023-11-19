62ad0f6bc21cbb70e5da5ee0acae0a651e5aaa00
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-11-19 10:31:10 +0000

    1.1

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-import express from "express";
 import bodyParser from "body-parser";
-import dotenv from "dotenv"; dotenv.config();
+import dotenv from "dotenv";
+import express from "express";
+const ffmpegPath = (await import('@ffmpeg-installer/ffmpeg')).path;
+import ffmpeg from 'fluent-ffmpeg'
+dotenv.config();
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+import axios from 'axios'
 import Bard from "bard-ai";
 import { BingChat } from "bing-chat-rnz";
 import { generateImagesLinks } from "bimg";
-import CharacterAI from "node_characterai";
-const characterAI = new CharacterAI();
-import { Spotify } from 'spotifydl-core'
+import { Spotify } from "spotifydl-core";
+
+try{
+const characterAI = await new ((await import("node_characterai")).default)();
+await characterAI.authenticateWithToken(process.env.CHARACTER_AI);
 
 const spotify = new Spotify({
   clientId: process.env.SPOTIFY_ID,
-  clientSecret: process.env.SPOTIFY_SECRET
-})
-await characterAI.authenticateWithToken(process.env.CHARACTER_AI)
-const app = express();
-
-app.listen(3000, () => {
-  console.log(`Example app listening on port 3000`);
+  clientSecret: process.env.SPOTIFY_SECRET,
 });
 
 const bard = new Bard({
@@ -26,8 +28,11 @@ const bard = new Bard({
 const sydneyAPI = new BingChat({
   cookie: process.env.BING_IMAGE_COOKIE,
 });
-
+const app = express();
 app.use(bodyParser.json());
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
 app.use("/api", (req, res, next) => {
   if (req.method !== "POST")
     return res.status(400).send("Forbidden, Use POST not GET");
@@ -39,35 +44,54 @@ app.use("/api", (req, res, next) => {
   next();
 });
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send(
+    "hello there, please see the gh repo for the endpoints because the dev is too lazy to add it here"
+  );
 });
-
 app.post("/api/bard", async (req, res) => {
-    let convo = bard.createChat(req.body.creds ? req.body.creds : "");
-    const resp = await convo.ask(req.body.query);
-    res.json({ creds: { ...convo.export() }, response: resp });
+  let convo = bard.createChat(req.body.creds ? req.body.creds : "");
+  const resp = await convo.ask(req.body.query);
+  res.json({ creds: { ...convo.export() }, response: resp });
 });
 
 app.post("/api/bimg", async (req, res) => {
-  try {
     res.send(await generateImagesLinks(req.body.query));
-  } catch (e) {
-    res.status(500).send(e);
-  }
 });
 app.post("/api/sydney", async (req, res) => {
-  try {
     const resp = await sydneyAPI.sendMessage(req.body.query);
     res.json({ response: resp.text });
-  } catch (e) {
-    res.status(500).send(e);
-  }
 });
 
-app.post("/api/cai/:characterid", async(req, res) => {
-  const {characterid} = req.params;
-  console.log(characterid)
+app.post("/api/cai/:characterid", async (req, res) => {
+  const { characterid } = req.params;
+  console.log(characterid);
   const chat = await characterAI.createOrContinueChat(characterid);
   const res2 = await chat.sendAndAwaitResponse(req.body.query, true);
-   res.json({data: {...res2}, response: (res2.text || res)})
+  res.json({ data: { ...res2 }, response: res2.text || res });
 });
+
+app.post("/api/spotify", async (req, res) => {
+  let url = req.body.query;
+  if (
+    !url.match(
+      /^(?:https?:\/\/(?:open|play)\.spotify\.com\/)(?:embed)?\/?(track)(?::|\/)((?:[0-9a-zA-Z]){22})/i
+    )
+  ) url = await axios
+      .get(
+        await axios
+          .get(url, {
+            maxRedirects: 0,
+            validateStatus: null,
+          })
+          .then((r) => r.headers.location)
+      )
+      .then((r) => r.data.split("\n")[249].split('"')[1]);
+
+  res.json({
+    details: { ...(await spotify.getTrack(url)) },
+    response: await spotify.downloadTrack(url),
+  });
+});
+}catch(e){
+  res.status(500).json({error: r.stack.toString()})
+}
