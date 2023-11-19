d1c7580d6ad47997cddfee59f8d3f6420e18e517
Gamers_indo1223 <76719536+gamersindo1223@users.noreply.github.com>
2023-11-19 15:01:55 +0000

    aw

--- index.js
+++ index.js
@@ -12,65 +12,81 @@ import { BingChat } from "bing-chat-rnz";
 import { generateImagesLinks } from "bimg";
 import { Spotify } from "spotifydl-core";
 import { createCaptchaSync } from "captcha-canvas";
-try {
-  const characterAI = await new (await import("node_characterai")).default();
-  await characterAI.authenticateWithToken(process.env.CHARACTER_AI);
+const characterAI = await new (await import("node_characterai")).default();
+await characterAI.authenticateWithToken(process.env.CHARACTER_AI);
 
-  const spotify = new Spotify({
-    clientId: process.env.SPOTIFY_ID,
-    clientSecret: process.env.SPOTIFY_SECRET,
-  });
+const spotify = new Spotify({
+  clientId: process.env.SPOTIFY_ID,
+  clientSecret: process.env.SPOTIFY_SECRET,
+});
 
-  const bard = new Bard({
-    "__Secure-1PSID": process.env.PSID_TOKEN,
-    "__Secure-1PSIDTS": process.env.PSIDTS_TOKEN,
-  });
-  const sydneyAPI = new BingChat({
-    cookie: process.env.BING_IMAGE_COOKIE,
-  });
-  const app = express();
-  app.use(bodyParser.json());
-  app.listen(process.env.PORT || 3000, () => {
-    console.log(`Example app listening on port ${process.env.PORT || 3000}`);
-  });
-  app.use("/api", (req, res, next) => {
-    if (req.method !== "POST")
-      return res.status(400).send("Forbidden, Use POST not GET");
-    if (!req.query.auth) return res.status(403).send("Forbidden, No Auth Key");
-    if (req.query.auth !== process.env.authkey)
-      return res.status(403).send("Forbidden, Invalid Key");
-    if (!req.body.query)
-      return res.status(400).send("Missing query parameters On Body");
-    next();
-  });
-  app.get("/", (req, res) => {
-    res.send(
-      "hello there, please see the gh repo for the endpoints because the dev is too lazy to add it here"
-    );
-  });
-  app.post("/api/bard", async (req, res) => {
+const bard = new Bard({
+  "__Secure-1PSID": process.env.PSID_TOKEN,
+  "__Secure-1PSIDTS": process.env.PSIDTS_TOKEN,
+});
+const sydneyAPI = new BingChat({
+  cookie: process.env.BING_IMAGE_COOKIE,
+});
+const app = express();
+app.use(bodyParser.json());
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening on port ${process.env.PORT || 3000}`);
+});
+app.use("/api", (req, res, next) => {
+  if (req.method !== "POST")
+    return res.status(400).send("Forbidden, Use POST not GET");
+  if (!req.query.auth) return res.status(403).send("Forbidden, No Auth Key");
+  if (req.query.auth !== process.env.authkey)
+    return res.status(403).send("Forbidden, Invalid Key");
+  if (!req.body.query)
+    return res.status(400).send("Missing query parameters On Body");
+  next();
+});
+app.get("/", (req, res) => {
+  res.send(
+    "hello there, please see the gh repo for the endpoints because the dev is too lazy to add it here"
+  );
+});
+app.post("/api/bard", async (req, res) => {
+  try {
     let convo = bard.createChat(req.body.creds ? req.body.creds : "");
     const resp = await convo.ask(req.body.query);
     res.json({ creds: { ...convo.export() }, response: resp });
-  });
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
 
-  app.post("/api/bimg", async (req, res) => {
-    res.send(await generateImagesLinks(req.body.query));
-  });
-  app.post("/api/sydney", async (req, res) => {
+app.post("/api/bimg", async (req, res) => {
+  try {
+    res.json({ response: await generateImagesLinks(req.body.query) });
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
+app.post("/api/sydney", async (req, res) => {
+  try {
     const resp = await sydneyAPI.sendMessage(req.body.query);
     res.json({ response: resp.text });
-  });
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
 
-  app.post("/api/cai/:characterid", async (req, res) => {
+app.post("/api/cai/:characterid", async (req, res) => {
+  try {
     const { characterid } = req.params;
     console.log(characterid);
     const chat = await characterAI.createOrContinueChat(characterid);
     const res2 = await chat.sendAndAwaitResponse(req.body.query, true);
     res.json({ data: { ...res2 }, response: res2.text || res });
-  });
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
 
-  app.post("/api/spotify", async (req, res) => {
+app.post("/api/spotify", async (req, res) => {
+  try {
     let url = req.body.query;
     if (
       !url.match(
@@ -92,11 +108,15 @@ try {
       details: { ...(await spotify.getTrack(url)) },
       response: await spotify.downloadTrack(url),
     });
-  });
-  app.post("/api/captcha", async (req, res) => {
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
+app.post("/api/captcha", async (req, res) => {
+  try {
     const { image, text } = createCaptchaSync(300, 100);
     res.json({ response: text, image: image });
-  });
-} catch (e) {
-  res.status(500).json({ error: r.stack.toString() });
-}
+  } catch (e) {
+    res.status(500).json({ error: r.stack.toString() });
+  }
+});
